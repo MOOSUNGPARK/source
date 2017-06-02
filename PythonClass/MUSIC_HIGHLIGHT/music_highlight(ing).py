8138f7f0462a99e1faf559937b6289c0f76f3e03
source <sunkid100000@gmail.com>
2017-06-02 11:01:38 +0900

    init

--- PythonClass/MUSIC_HIGHLIGHT/music_highlight(ing).py
+++ PythonClass/MUSIC_HIGHLIGHT/music_highlight(ing).py
@@ -10,8 +10,8 @@ import os
 
 ################## 플레이 정보 ##################
 play_duration = 10                # 노래 재생 시간
-file_loc = 'c:/python/data/music' # 노래 폴더 위치
-show_chroma = False                # 크로마그램 출력
+file_loc = 'c:\python\data\music' # 노래 폴더 위치
+show_chroma = True               # 크로마그램 출력
 ###############################################
 
 
