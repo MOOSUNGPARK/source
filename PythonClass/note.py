16cf4dbdf5e91219807fbc442456940f544cb2a2
MOOSUNGPARK <sunkid100000@gmail.com>
2017-06-03 18:01:48 +0900

    z

--- PythonClass/note.py
+++ PythonClass/note.py
@@ -1,47 +1,8 @@
-from multiprocessing import Process, Queue
-import time as tm
-def MultiProcess(func,arg):
-    procs = []
-    for i in range(3):
-        p = Process(target=func, args=(arg, ))
-        procs.append(p)
-    for p in procs:
-        p.join()
-
-def do_work(start, end, result):
-    sum = 0
-    for i in range(start, end):
-        sum += i
-    result.put(sum)
-    return
+MATRIX = 5
+PITCH_LOCATION = "| " + "{:^6s} | " * MATRIX  # 투구 영역
+PITCH_LOCATION = (PITCH_LOCATION + '\n') * MATRIX
+PITCH_LOCATION = "---------" * MATRIX + "\n" + PITCH_LOCATION + "---------" * MATRIX
+print(PITCH_LOCATION.format(*[str(idx) for idx in range(26)]))  # 투구 영역 5 * 5 출력
 
 
-s_time = tm.time()
-if __name__ == '__main__':
-
-    START, END = 0, 20000000
-    result = Queue()
-    pr1 = Process(target=do_work, args=(START, END / 2, result))
-    pr2 = Process(target=do_work, args=(END / 2, END, result))
-    pr1.start()
-    pr2.start()
-    pr1.join()
-    pr2.join()
-    result.put('STOP')
-    sum = 0
-    while True:
-        tmp = result.get()
-        if tmp == 'STOP':
-            break
-        else:
-            sum += tmp
-
-print('Result : ', sum, 'time = ', tm.time() - s_time)
-
-def MultiProcess(func,arg):
-    procs = []
-    for i in range(3):
-        p = Process(target=func, args=(arg, ))
-        procs.append(p)
-    for p in procs:
-        p.join()
+print(*[str(idx) for idx in range(26)])
