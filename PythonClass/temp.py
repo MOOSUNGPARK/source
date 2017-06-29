5f02ced171f337e60c31897655e006996b9c528e
source <sunkid100000@gmail.com>
2017-06-29 18:09:48 +0900

    init

--- PythonClass/temp.py
+++ PythonClass/temp.py
@@ -1,22 +1,103 @@
 from selenium import webdriver
 from urllib.request import urlretrieve
-from selenium.webdriver.common.keys import Keys
+import os
 from bs4 import BeautifulSoup
-import time
-from PIL import Image
+import io
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 
 
-binary = 'c:\chromedriver/chromedriver.exe'
-browser = webdriver.Chrome(binary)
-browser.get("http://www.simplyscripts.com/tv_all.html")
 
-html = browser.page_source
-soup = BeautifulSoup(html, "html.parser")
-content_find = soup.find_all("td", class_="list-subject")  # len(18)
-for idx, tag in enumerate(content_find,1):
-    # print(tag.a['href'])
-    splittag = tag.a['href'].split('&')
-    # print(splittag)
-    downloadtag = 'http://cineaste.co.kr/bbs/download.php?bo_table=psd_caption&' + splittag[1] + '&no=0&ds=1&js=on&' + splittag[2] + '&' + splittag[3]
-    urlretrieve(downloadtag,'d:/data/{}.txt'.format(idx))
-    print(downloadtag)
+class SubtitleCrawler:
+    __CHROME_DRIVER_PATH = 'c:\\chromedriver\\'
+    __SAVE_FILE_PATH = 'd:\\data\\'
+
+    def __init__(self):
+        self._sub_url = 'http://www.simplyscripts.com/movie-screenplays.html'
+        self._download_url = []
+        self._set_chrome_driver()
+
+    def _set_chrome_driver(self):
+        self.driver = webdriver.Chrome(SubtitleCrawler.__CHROME_DRIVER_PATH + 'chromedriver')
+
+    def _get_sub_url(self):
+        self.driver.get(self._sub_url)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        body = soup.find_all('p')
+
+        for idx, tag in enumerate(body):
+            try:
+                url = tag.a['href']
+                extension = url.split('.')[-1].lower()
+                self._make_save_dir(extension)
+                self._download_url.append([url,extension])
+            except:
+                print('no.'+str(idx),'url load failed')
+
+    def _sub_downloads(self):
+        for idx, urlextension in enumerate(self._download_url):
+            try :
+                url = urlextension[0]
+                extension = urlextension[1]
+
+                if extension in ('html', 'htm'):
+                    urlretrieve(url, SubtitleCrawler.__SAVE_FILE_PATH + 'txt\\m{}.txt'.format(idx))
+                else:
+                    urlretrieve(url, SubtitleCrawler.__SAVE_FILE_PATH + '{0}\\m{1}.{0}'.format(extension, idx))
+                if idx/10 == idx//10 :
+                    print('no.'+str(idx), 'subtitle downloading')
+
+            except :
+                print('no.'+str(idx), 'subtitle downloading failed')
+
+    def _make_save_dir(self,extension):
+        save_path = SubtitleCrawler.__SAVE_FILE_PATH + '{}\\'.format(extension)
+        if not os.path.isdir(os.path.split(save_path)[0]) and extension not in ('html', 'htm'):
+            os.mkdir(os.path.split(save_path)[0])
+
+    def _pdf_to_txt(self, pdf_file_path):
+        filenames = os.listdir(pdf_file_path)  # 지정된 폴더 내 파일이름들 불러오기
+        full_filenames = []
+        for filename in filenames:
+            full_filenames.append(os.path.join(pdf_file_path, filename))  # full_filename = 경로+파일이름
+
+        for full_filename in full_filenames:
+            rsrcmgr = PDFResourceManager()
+            retstr = io.StringIO()
+            codec = 'utf-8'
+            laparams = LAParams()
+            device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            password = ""
+            maxpages = 0
+            caching = True
+            pagenos = set()
+            fp = open(full_filename, 'rb')
+            for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                          password=password,
+                                          caching=caching,
+                                          check_extractable=True):
+                interpreter.process_page(page)
+
+            text = retstr.getvalue()
+
+            fp.close()
+            device.close()
+            retstr.close()
+            # return text
+            ital = open('d://italianjob.txt', 'w', encoding='UTF-8', newline='')
+            ital.write(text)
+            ital.close()
+
+    def play_crawler(self):
+        print('crawling start.')
+        self._get_sub_url()
+        print('crawling complete.')
+        print('subtitle downloading.')
+        self._sub_downloads()
+        print('downloading complete.')
+
+crawler = SubtitleCrawler()
+crawler.play_crawler()
