29c9e4115a29b248fd8dcb2e81e17828e5c6f18b
source <sunkid100000@gmail.com>
2017-07-17 15:43:32 +0900

    init

--- PythonClass/DEEP_LEARNING/3.6. mnist.py
+++ PythonClass/DEEP_LEARNING/3.6. mnist.py
@@ -1,6 +1,6 @@
 import sys, os
-sys.path.insert(0,os.getcwd())
+sys.path.insert(0, os.getcwd())
 # from mnist import load_mnist
-
+import dataset.mnist as mnist
 
 print(sys.path)
--- PythonClass/note.py
+++ PythonClass/note.py
@@ -1,13 +1,3 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.arange(0, 12, 0.1)
-print(t)
-
-plt.figure()
-plt.plot(t)
-plt.grid()
-plt.xlabel('size')
-plt.ylabel('cost')
-plt.title('size & cost')
-plt.show()
