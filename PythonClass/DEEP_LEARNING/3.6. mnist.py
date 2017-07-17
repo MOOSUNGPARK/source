ef98ac1faa68ee0df6b7ab2ef1d0db9cc832108a
source <sunkid100000@gmail.com>
2017-07-17 16:03:30 +0900

    init

--- PythonClass/DEEP_LEARNING/3.6. mnist.py
+++ PythonClass/DEEP_LEARNING/3.6. mnist.py
@@ -1,6 +1,5 @@
 import sys, os
-sys.path.insert(0, os.getcwd())
-# from mnist import load_mnist
-import dataset.mnist as mnist
+sys.path.append(os.getcwd())
+sys.path.append('C:\\python\\deep-learning-from-scratch')
 
 print(sys.path)
--- PythonClass/note.py
+++ PythonClass/note.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+chi = np.loadtxt('c:\\python\\data\\창업건수.csv', skiprows=1, unpack=True, delimiter=',')
+print(chi)
+x = chi[0]
+y = chi[4]
+plt.figure()
+plt.plot(x,y)
+plt.xlabel('year')
+plt.ylabel('chicken')
+plt.title('year & chicken')
+plt.show()
+
+
