cd82605105312203b4d4be30e9b25d3b477131fc
source <sunkid100000@gmail.com>
2017-07-27 18:21:23 +0900

    init

--- PythonClass/note.py
+++ PythonClass/note.py
@@ -1,27 +1,56 @@
 import numpy as np
-import copy
 
-def Relu(x):
-    return np.maximum(x,0)
+class Affine:
+    def __init__(self, w, b):
+        self.x = None
+        self.w = w
+        self.b = b
+        self.dW = None
+        self.db = None
 
-x = np.array([[1,2],[-1,-9],[2,-4]])
-print(Relu(x))
+    def forward(self, x):
+        self.x = x
+        return np.dot(self.x,self.w) + self.b
 
-
+    def backward(self, x, out):
+        self.x = x
+        dx = np.dot(out, self.w.T)
+        dw = np.dot(self.x.T, out)
+        db = np.sum(out, axis=0)
+        return dx, dw, db
 
 class Relu:
     def __init__(self):
         self.mask = None
 
-    def forward(self, x):
-        self.mask = (x<=0)
-        return np.maximum(x,0)
+    def forward(self,x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
 
     def backward(self, dout):
         dout[self.mask] = 0
         dx = dout
         return dx
 
-x = np.array([[1.0, -0.5], [-2.0, 3.0]])
-relulayer = Relu()
-print(relulayer.forward(x))
+
+x = np.array([1,2], ndmin=2)
+w1 = np.array([[1,3,5],[2,4,6]])
+b1 = np.array([1,2,3])
+w2 = np.array([[1,4],[2,5],[3,6]])
+b2 = np.array([1,2])
+
+affine1 = Affine(w1,b1)
+relu = Relu()
+affine2 = Affine(w2,b2)
+h = affine1.forward(x)
+h2 = relu.forward(h)
+out = affine2.forward(h2)
+
+print(out)
+
+dx2, dw2, db2 = affine2.backward(h2, out)
+dh2 = relu.backward(dx2)
+dx1, dw1, db1 = affine1.backward(x, dh2)
+print(dx1, dw1, db1)
