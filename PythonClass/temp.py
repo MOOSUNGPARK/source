053f0f9f993f024fa643fa668fe5027c30687099
source <sunkid100000@gmail.com>
2017-07-31 14:59:00 +0900

    init

--- PythonClass/temp.py
+++ PythonClass/temp.py
@@ -1,35 +1,151 @@
+# coding: utf-8
+import sys,os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
 
-# 차원 명시해줘야 에러가 안남
-x = np.array([[1, 2],[2,4]])
-w = np.array([[1, 3, 5], [2, 4, 6]])
-b = np.array([1, 2, 3])
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
 
+    def update(self, params, grads):
+        if self.v is None :
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
 
-class Affine:
-    def __init__(self, w, b):
-        self.x = None
-        self.w = w
-        self.b = b
-        self.dW = None
-        self.db = None
-
-    def forward(self, x):
-        self.x = x
-        return np.dot(self.x, self.w) + self.b
-
-    def backward(self, x, out):
-        self.x = x
-        dx = np.dot(out, self.w.T)
-        dw = np.dot(self.x.T, out)
-        db = np.sum(out, axis=0)
-        return dx, dw, db
-
-
-affine1 = Affine(w, b)
-out = affine1.forward(x)
-print(out)
-print(affine1.backward(x, out))
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
 
 
 
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+        self.momentum = Momentum()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+    # x : 입력 데이터, t : 정답 레이블
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1: t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+    # x : 입력 데이터, t : 정답 레이블
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+
+
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        # 결과 저장
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+        return grads
+
+# 데이터 읽기
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+
+# 하이퍼파라미터
+iters_num = 10000  # 반복 횟수를 적절히 설정한다.
+train_size = x_train.shape[0] # 60000 개
+batch_size = 100  # 미니배치 크기
+learning_rate = 0.1
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+
+# 1에폭당 반복 수
+iter_per_epoch = max(train_size / batch_size, 1)
+print(iter_per_epoch) # 600
+
+for i in range(iters_num): # 10000
+    # 미니배치 획득  # 랜덤으로 100개씩 뽑아서 10000번을 수행하니까 백만번
+    batch_mask = np.random.choice(train_size, batch_size) # 100개 씩 뽑아서 10000번 백만번
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+
+
+    # 기울기 계산
+    #grad = network.numerical_gradient(x_batch, t_batch)
+    grad = network.gradient(x_batch, t_batch)
+    # 매개변수 갱신
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    network.momentum.update(network.params, grad)
+
+
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss) # cost 가 점점 줄어드는것을 보려고
+    # 1에폭당 정확도 계산 # 여기는 훈련이 아니라 1에폭 되었을때 정확도만 체크
+
+    if i % iter_per_epoch == 0: # 600 번마다 정확도 쌓는다.
+        print(x_train.shape) # 60000,784
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc) # 10000/600 개  16개 # 정확도가 점점 올라감
+        test_acc_list.append(test_acc)  # 10000/600 개 16개 # 정확도가 점점 올라감
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+# 그래프 그리기
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
