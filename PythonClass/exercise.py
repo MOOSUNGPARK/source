d768ddf5201944ee91829cc582ab002e520d73ef
source <sunkid100000@gmail.com>
2017-08-08 18:45:47 +0900

    init

--- PythonClass/exercise.py
+++ PythonClass/exercise.py
@@ -1,146 +1,101 @@
-# coding: utf-8
-import sys,os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import tensorflow as tf
 import numpy as np
-from common.layers import *
-from common.gradient import numerical_gradient
-from collections import OrderedDict
-import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 
-class SimpleConvNet:
+###### mnist 데이터 불러오기 및 정제 #####
 
-    def __init__(self, input_dim=(1,28,28),
-                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
-                 hidden_size=100, output_size=10, weight_init_std=0.01):
-        filter_num = conv_param['filter_num']
-        filter_size = conv_param['filter_size']
-        filter_pad = conv_param['pad']
-        filter_stride = conv_param['stride']
-        input_size = input_dim[1]
-        conv_output_size = ((input_size - filter_size + 2*filter_pad) / filter_stride) + 1
-        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+############################################
+# mnist 데이터 중 10000개 저장
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, one_hot_label=True)
+# input = np.concatenate((x_train, x_test), axis=0)
+# target = np.concatenate((t_train, t_test), axis=0)
+# print('input shape :', input.shape, '| target shape :', target.shape)
+# a = np.concatenate((input, target), axis=1)
+# np.savetxt('mnist.csv', a[:10000], delimiter=',')
+############################################
 
-        self.params = {}
-        self.params['W1'] = weight_init_std * \
-                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
-        self.params['b1'] = np.zeros(filter_num)
-        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
-        self.params['b2'] = np.zeros(hidden_size)
-        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b3'] = np.zeros(output_size)
+# 파일 로드 및 변수 설정
+mnist = np.loadtxt('mnist.csv', delimiter=',', unpack=False, dtype='float32')
+print(mnist.shape)
 
-        self.layers = OrderedDict()
-        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
-                                           conv_param['stride'], conv_param['pad'])
-        self.layers['Relu1'] = Relu()
-        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
-        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
-        self.layers['Relu2'] = Relu()
-        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+train_num = int(mnist.shape[0] * 0.9)
 
-        self.lastlayer = SoftmaxWithLoss()
+x_train, x_test = mnist[:train_num,:784], mnist[train_num:,:784]
+t_train, t_test = mnist[:train_num,784:], mnist[train_num:,784:]
+
+print('x train shape :',x_train.shape, '| x target shape :',x_test.shape)
+print('t train shape :',t_train.shape, '| t target shape :',t_test.shape)
+
+global_step = tf.Variable(0, trainable=False, name='global_step')
+X = tf.placeholder(tf.float32)
+T = tf.placeholder(tf.float32)
+# W = tf.get_variable(name='W', shape=[784, 10], initializer=tf.contrib.layers.variance_scaling_initializer()) # he 초기값
+W = tf.Variable(tf.random_uniform([784,10], -1e-7, 1e-7)) # [784,10] 형상을 가진 -1~1 사이의 균등분포 어레이
+
+b = tf.Variable(tf.random_uniform([10], -1e-7, 1e-7))    # [10] 형상을 가진 -1~1 사이의 균등분포 벡터
+Y = tf.add(tf.matmul(X,W), b)
+
+############################################
+# 그외 가중치 초기화 방법
+# W = tf.Variable(tf.random_uniform([784,10], -1, 1)) # [784,10] 형상을 가진 -1~1 사이의 균등분포 어레이
+# W = tf.get_variable(name="W", shape=[784, 10], initializer=tf.contrib.layers.xavier_initializer()) # xavier 초기값
+# b = tf.Variable(tf.zeros([10]))
+############################################
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=T, logits=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.05).minimize(cost, global_step=global_step)
+
+############################################
+# 그외 옵티마이저
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+# optimizer = tf.train.AdagradOptimizer(learning_rate=0.01)
+# optimizer = tf.train.MomentumOptimizer(learning_rate=0.01)
+############################################
+
+sess = tf.Session()
+saver = tf.train.Saver(tf.global_variables())
+
+cp = tf.train.get_checkpoint_state('./save')
+if cp and tf.train.checkpoint_exists(cp.model_checkpoint_path):
+    saver.restore(sess, cp.model_checkpoint_path)
+    print(sess.run(global_step),'회 학습한 데이터 로드 완료')
+else:
+    sess.run(tf.global_variables_initializer())
+    print('새로운 학습 시작')
 
 
-    def predict(self, x):
-        for layer in self.layers.values():
-            x = layer.forward(x)
-        return x
-    # x : 입력 데이터, t : 정답 레이블
+# epoch, batch 설정
+epoch = 1000
+batch_size = 100
+total_batch = int(x_train.shape[0] / batch_size)
 
-    def loss(self, x, t):
-        y = self.predict(x)
-        return self.lastlayer.forward(y, t)
+for each_epoch in range(epoch):
+    total_cost = 0
 
-    def accuracy(self, x, t):
-        y = self.predict(x)
-        y = np.argmax(y, axis=1)
-        if t.ndim != 1: t = np.argmax(t, axis=1)
-        accuracy = np.sum(y == t) / float(x.shape[0])
-        return accuracy
-    # x : 입력 데이터, t : 정답 레이블
+    for idx in range(0, total_batch, batch_size):
+        # 텐서플로우의 mnist 모델의 next_batch 함수를 이용해
+        # 지정한 크기만큼 학습할 데이터를 가져옵니다.
+        batch_x, batch_y = x_train[idx : idx+batch_size], t_train[idx : idx+batch_size]
 
-    def numerical_gradient(self, x, t):
-        loss_W = lambda W: self.loss(x, t)
-        grads = {}
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
-        return grads
+        _, cost_val = sess.run([optimizer, cost], feed_dict={X : batch_x, T : batch_y})
+        total_cost += cost_val
 
-    def gradient(self, x, t):
-        # forward
-        self.loss(x, t)
-        # backward
-        dout = 1
-        dout = self.lastlayer.backward(dout)
-        layers = list(self.layers.values())
-        layers.reverse()
-        for layer in layers:
-            dout = layer.backward(dout)
-        # 결과 저장
-        grads = {}
-        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
-        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
-        return grads
-#
-# # 데이터 읽기
-# (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-#
-#
-# # 하이퍼파라미터
-# iters_num = 10000  # 반복 횟수를 적절히 설정한다.
-# train_size = x_train.shape[0] # 60000 개
-# batch_size = 100  # 미니배치 크기
-# learning_rate = 0.1
-# train_loss_list = []
-# train_acc_list = []
-# test_acc_list = []
-#
-#
-# # 1에폭당 반복 수
-# iter_per_epoch = max(train_size / batch_size, 1)
-# print(iter_per_epoch) # 600
-#
-# for i in range(iters_num): # 10000
-#     # 미니배치 획득  # 랜덤으로 100개씩 뽑아서 10000번을 수행하니까 백만번
-#     batch_mask = np.random.choice(train_size, batch_size) # 100개 씩 뽑아서 10000번 백만번
-#     x_batch = x_train[batch_mask]
-#     t_batch = t_train[batch_mask]
-#
-#
-#     # 기울기 계산
-#     #grad = network.numerical_gradient(x_batch, t_batch)
-#     grad = network.gradient(x_batch, t_batch)
-#     # 매개변수 갱신
-#
-#     for key in ('W1', 'b1', 'W2', 'b2'):
-#         network.params[key] -= learning_rate * grad[key]
-#
-#
-#     # 학습 경과 기록
-#     loss = network.loss(x_batch, t_batch)
-#     train_loss_list.append(loss) # cost 가 점점 줄어드는것을 보려고
-#     # 1에폭당 정확도 계산 # 여기는 훈련이 아니라 1에폭 되었을때 정확도만 체크
-#
-#     if i % iter_per_epoch == 0: # 600 번마다 정확도 쌓는다.
-#         print(x_train.shape) # 60000,784
-#         train_acc = network.accuracy(x_train, t_train)
-#         test_acc = network.accuracy(x_test, t_test)
-#         train_acc_list.append(train_acc) # 10000/600 개  16개 # 정확도가 점점 올라감
-#         test_acc_list.append(test_acc)  # 10000/600 개 16개 # 정확도가 점점 올라감
-#         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
-#
-#
-# # 그래프 그리기
-# markers = {'train': 'o', 'test': 's'}
-# x = np.arange(len(train_acc_list))
-# plt.plot(x, train_acc_list, label='train acc')
-# plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-# plt.xlabel("epochs")
-# plt.ylabel("accuracy")
-# plt.ylim(0, 1.0)
-# plt.legend(loc='lower right')
-# plt.show()
+    print('Epoch:', '%04d' % (each_epoch + 1),
+          'Avg. cost =', '{:.8f}'.format(total_cost / total_batch),
+          )
+
+print('최적화 완료!')
+
+# 최적화가 끝난 뒤, 변수를 저장합니다.
+saver.save(sess, './save/mnist_dnn.ckpt', global_step=global_step)
+
+#########
+# 결과 확인
+######
+# model 로 예측한 값과 실제 레이블인 Y의 값을 비교합니다.
+# tf.argmax 함수를 이용해 예측한 값에서 가장 큰 값을 예측한 레이블이라고 평가합니다.
+# 예) [0.1 0 0 0.7 0 0.2 0 0 0 0] -> 3
+correct_prediction = tf.equal(tf.argmax(T, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Train 정확도 :', sess.run(accuracy, feed_dict={X: x_train, T: t_train}))
+print('Test 정확도:', sess.run(accuracy, feed_dict={X: x_test, T: t_test}))
--- PythonClass/note.py
+++ PythonClass/note.py
@@ -1,11 +1,101 @@
-# 알파고를 만든 구글의 딥마인드의 논문을 참고한 DQN 모델을 생성합니다.
-# http://www.nature.com/nature/journal/v518/n7540/full/nature14236.html
 import tensorflow as tf
 import numpy as np
-import random
-from collections import deque
+from dataset.mnist import load_mnist
 
-from tensorflow.examples.tutorials.mnist import input_data
+##### mnist 데이터 불러오기 및 정제 #####
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-print(mnist)
+############################################
+# mnist 데이터 중 10000개 저장
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, one_hot_label=True)
+# input = np.concatenate((x_train, x_test), axis=0)
+# target = np.concatenate((t_train, t_test), axis=0)
+# print('input shape :', input.shape, '| target shape :', target.shape)
+# a = np.concatenate((input, target), axis=1)
+# np.savetxt('mnist.csv', a[:10000], delimiter=',')
+############################################
+
+# 파일 로드 및 변수 설정
+mnist = np.loadtxt('mnist.csv', delimiter=',', unpack=False, dtype='float32')
+print(mnist.shape)
+
+train_num = int(mnist.shape[0] * 0.9)
+
+x_train, x_test = mnist[:train_num,:784], mnist[train_num:,:784]
+t_train, t_test = mnist[:train_num,784:], mnist[train_num:,784:]
+
+print('x train shape :',x_train.shape, '| x target shape :',x_test.shape)
+print('t train shape :',t_train.shape, '| t target shape :',t_test.shape)
+
+global_step = tf.Variable(0, trainable=False, name='global_step')
+X = tf.placeholder(tf.float32,[None, 784])
+T = tf.placeholder(tf.float32,[None, 10])
+W = tf.Variable(tf.random_uniform([784,10], -1e-7, 1e-7)) # [784,10] 형상을 가진 -1e-7 ~ 1e-7 사이의 균등분포 어레이
+b = tf.Variable(tf.random_uniform([10], -1e-7, 1e-7))    # [10] 형상을 가진 -1~1 사이의 균등분포 벡터
+Y = tf.add(tf.matmul(X,W), b) # tf.matmul(X,W) + b 와 동일
+
+############################################
+# 그외 가중치 초기화 방법
+# W = tf.Variable(tf.random_uniform([784,10], -1, 1)) # [784,10] 형상을 가진 -1~1 사이의 균등분포 어레이
+# W = tf.get_variable(name="W", shape=[784, 10], initializer=tf.contrib.layers.xavier_initializer()) # xavier 초기값
+# W = tf.get_variable(name='W', shape=[784, 10], initializer=tf.contrib.layers.variance_scaling_initializer()) # he 초기값
+# b = tf.Variable(tf.zeros([10]))
+############################################
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=T, logits=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.05).minimize(cost, global_step=global_step)
+
+############################################
+# 그외 옵티마이저
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+# optimizer = tf.train.AdagradOptimizer(learning_rate=0.01)
+# optimizer = tf.train.MomentumOptimizer(learning_rate=0.01)
+############################################
+
+
+##### mnist 학습시키기 #####
+sess = tf.Session()
+saver = tf.train.Saver(tf.global_variables())
+
+
+cp = tf.train.get_checkpoint_state('./save') # save 폴더를 checkpoint로 설정
+# checkpoint가 설정되고, 폴더가 실제로 존재하는 경우 restore 메소드로 변수, 학습 정보 불러오기
+if cp and tf.train.checkpoint_exists(cp.model_checkpoint_path):
+    saver.restore(sess, cp.model_checkpoint_path)
+    print(sess.run(global_step),'회 학습한 데이터 로드 완료')
+# 그렇지 않은 경우 일반적인 sess.run()으로 tensorflow 실행
+else:
+    sess.run(tf.global_variables_initializer())
+    print('새로운 학습 시작')
+
+# epoch, batch 설정
+epoch = 1000
+batch_size = 100
+total_batch = int(x_train.shape[0] / batch_size)
+
+# 설정한 epoch 만큼 루프
+for each_epoch in range(epoch):
+    total_cost = 0
+
+    # 각 epoch 마다 batch 크기만큼 데이터를 뽑아서 학습
+    for idx in range(0, total_batch, batch_size):
+        batch_x, batch_y = x_train[idx : idx+batch_size], t_train[idx : idx+batch_size]
+
+        _, cost_val = sess.run([optimizer, cost], feed_dict={X : batch_x, T : batch_y})
+        total_cost += cost_val
+
+    print('Epoch:', '%04d' % (each_epoch + 1),
+          'Avg. cost =', '{:.8f}'.format(total_cost / total_batch),
+          )
+
+print('최적화 완료!')
+
+# 최적화가 끝난 뒤, 변수와 학습 정보 저장
+saver.save(sess, './save/mnist_dnn.ckpt', global_step=global_step)
+
+##### 학습 결과 확인 #####
+# equal 메소드로 (True, False, True, ....) 형식으로 출력
+correct_prediction = tf.equal(tf.argmax(T, 1), tf.argmax(Y, 1))
+# tf.cast로 True, False 등을 float32 형태로 변경 True -> 1.0, False -> 0.0
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Train 정확도 :', sess.run(accuracy, feed_dict={X: x_train, T: t_train}))
+print('Test 정확도:', sess.run(accuracy, feed_dict={X: x_test, T: t_test}))
