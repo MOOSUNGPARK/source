d6261baa8a1ae6bf4d336aad5f256cda7dc50862
pms <sunkid100000@gmail.com>
2018-06-05 14:14:17 +0900

    z

--- PythonClass/temp.py
+++ PythonClass/temp.py
@@ -1,8 +1,11 @@
-import numpy as np
+import tensorflow as tf
 
-a = [[1,2],
-     [3,4]]
-b = [[5,6],
-     [7,8]]
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 
-print(np.sum([a,b], axis=0))
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+
+c = tf.matmul(a, b)
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+print(sess.run(c))
