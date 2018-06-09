2b94e905910f9413551d041a4a617b4c6d945b68
pms <sunkid100000@gmail.com>
2018-06-10 02:48:43 +0900

    z

--- PythonClass/temp.py
+++ PythonClass/temp.py
@@ -1,1 +1,30 @@
 import os
+import numpy as np
+#
+
+# def rgb(pred_img, color):
+#     rgb_dic = {'green': 0, 'blue': 1, 'red': 2}
+#     rgb_list = [np.zeros([1, 2, 2, 1]) for _ in range(3)]
+#     rgb_list[rgb_dic[color]] = pred_img
+#
+#     return rgb_list
+
+# rgb_dic = {'green': 0, 'blue': 1, 'red': 2}
+# print(rgb_dic['red'])
+# rgb_list = [ np.zeros([1, 2, 2, 1]),
+#              np.zeros([1, 2, 2, 1]),
+#              np.zeros([1, 2, 2, 1])]
+# rgb_list2 = [ np.zeros([1, 2, 2, 1]) for _ in range(3)]
+# print(np.shape(rgb_list))
+# print(np.shape(rgb_list2))
+
+
+
+# a = [[[1,2,3,4], [1,2,3,4]],[[1,2,3,4], [1,2,3,4]]]
+# print(np.shape(a))
+# print(len(np.shape(a)))
+
+a, b, c = [], [], []
+print(a)
+print(b)
+print(c)
